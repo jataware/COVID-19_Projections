0a33bc2054d3e02beadbd3b9f8bcd298e55d7f43
Five Grant <5@fivegrant.com>
2022-08-30 08:28:46 -0500

    Move model into a separate directory

--- compartmentalized_model/src/input.jl
+++ compartmentalized_model/src/input.jl
@@ -17,31 +17,27 @@ const state = SVector(NTuple((;
 const total_population = sum(state);
 
 const parameters = SVector(NTuple((;
-    βI=0,
-    βA=0, 
-    βQ=0, 
-    βL=0, 
-    βE₂=0, 
-    m=0, 
-    ζ=0, 
-    κ₁=0, 
-    ρκ₂=0, 
-    ρ=0, 
-    q=0,
-    τI=0, 
-    τA=0, 
-    γI=0, 
-    γA=0, 
-    γQ=0, 
-    γL=0,
-    δI=0, 
-    δL=0, 
-    δQ=0, 
+    βI = 0,
+    βA = 0, 
+    βQ = 0, 
+    βL = 0, 
+    βE₂ = 0, 
+    m = 0, 
+    ζ = 0, 
+    κ₁ = 0, 
+    ρκ₂ = 0, 
+    ρ = 0, 
+    q = 0,
+    τI = 0, 
+    τA = 0, 
+    γI = 0, 
+    γA = 0, 
+    γQ = 0, 
+    γL = 0,
+    δI = 0, 
+    δL = 0, 
+    δQ = 0, 
     total_population = total_population
 )))
-
-const total = sum(state);
-
-
 
 end

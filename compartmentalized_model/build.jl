b111b4b7f31fecf0b36daa0431267ba956bd4323
Five Grant <5@fivegrant.com>
2022-08-30 17:15:29 -0500

    Create compile script

---
+++ compartmentalized_model/build.jl
@@ -0,0 +1,19 @@
+#!/usr/bin/env julia
+
+if length(ARGS) == 0
+    println("Error: Pass in name of directory to be created.")
+    exit()
+end
+
+println("Starting installation")
+import Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
+println("Building model")
+println("(warning: this may take a few minutes)")
+import PackageCompiler
+PackageCompiler.create_app(".",ARGS[1])
+
+println("Model finished:")
+println("Run by using `$(ARGS[1])/bin/COVID19Projections`")
